58a5e412132eb0912aab3a51fc8e5b688e71fad2
tui62689 <112034701+tui62689@users.noreply.github.com>
2024-10-14 15:54:03 -0400

    Moved items from Main Activity to Fragment

--- app/src/main/java/edu/temple/fragmentrefactor/Fragment.kt
+++ app/src/main/java/edu/temple/fragmentrefactor/Fragment.kt
@@ -5,16 +5,33 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
 
 
 class Fragment : Fragment() {
+
 
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
+        val view = inflater.inflate(R.layout.fragment_, container, false)
+        val displayTextView = view.findViewById<TextView>(R.id.displayTextView)
+        val nameEditText = view.findViewById<EditText>(R.id.nameEditText)
+        val changeButton = view.findViewById<Button>(R.id.changeButton)
+
+        changeButton.setOnClickListener {
+            val name = nameEditText.text
+
+            displayTextView.text = if (name.isNotBlank()) {
+                "Hello, $name!"
+            } else {
+                "Please enter your name"
+            }
+        }
         return inflater.inflate(R.layout.fragment_, container, false)
     }
 
